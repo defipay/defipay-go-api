f852ac3a55b917e7dbea3f9453e93f5ba9f37255
DefiPay <105914570+defipay@users.noreply.github.com>
2022-07-11 12:56:31 +0800

    Update env.go

--- defipay/env.go
+++ defipay/env.go
@@ -6,9 +6,9 @@ type Env struct {
 }
 
 func Sandbox() Env {
-	return Env{Host: "http://api-test.defipay.biz/api-service", PubKey: "03412208e920ba78d97c33c1476db11506cb6d4b3fc218a8207ca523c8d392a3f0"}
+	return Env{Host: "https://api-test.defipay.biz/api-service", PubKey: "02a17fffb024cce6220ddf91b40711dc15fd8f830e23f6160c6a4eac8bc0eba820"}
 }
 
 func Prod() Env {
-	return Env{Host: "https://api.custody.cobo.com", PubKey: "02c3e5bacf436fbf4da78597e791579f022a2e85073ae36c54a361ff97f2811376"}
+	return Env{Host: "https://api.defipay.biz/api-service", PubKey: "02adb46f0c10b5ec51d0df2183a812fdf7b330ef2c948e36cdb479f1af73a22753"}
 }
